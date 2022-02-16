4ce8535dab3b87e3eefefee4e600b8e6b000bd13
inmacc96 <inmacaballerocarrero@gmail.com>
2022-02-16 12:33:09 +0100

    Added PatientsList component.

--- patient-manager/src/App.jsx
+++ patient-manager/src/App.jsx
@@ -1,11 +1,13 @@
 import Form from "./components/Form";
 import Header from "./components/Header";
+import PatientsList from "./components/PatientsList";
 
 function App() {
   return (
     <>
       <Header />
       <Form />
+      <PatientsList />
     </>
   );
 }
---
+++ patient-manager/src/components/PatientsList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PatientsList() {
+  return (
+    <div>
+        <h2>Listado Pacientes</h2>
+    </div>
+  )
+}
+
+export default PatientsList
