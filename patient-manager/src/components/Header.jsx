4ba5c6251eaa7b8eb72bfba8a75806f8578a9dca
inmacc96 <inmacaballerocarrero@gmail.com>
2022-02-16 13:38:20 +0100

    Applied styles to the header.

--- patient-manager/src/components/Header.jsx
+++ patient-manager/src/components/Header.jsx
@@ -1,7 +1,10 @@
-function Header(){
-    return(
-        <h1>Desde Header</h1>
-    )
+function Header() {
+  return (
+    <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
+      Seguimiento Pacientes {""}
+      <span className="text-indigo-600">Veterinaria</span>
+    </h1>
+  );
 }
 
 export default Header;
