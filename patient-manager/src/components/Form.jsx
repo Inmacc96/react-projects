18021ed268e9c17cd9ef0562ced46332e9e1348d
inmacc96 <inmacaballerocarrero@gmail.com>
2022-02-16 14:22:52 +0100

    Incorporated add patient form.

--- patient-manager/src/components/Form.jsx
+++ patient-manager/src/components/Form.jsx
@@ -3,10 +3,96 @@ function Form() {
     <div className="md:w-1/2 lg:w-2/5">
       <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
 
-      <p className="text-lg mt-5 text-center">
-        Añade Pacientes y {''}
+      <p className="text-lg mt-5 text-center mb-10">
+        Añade Pacientes y {""}
         <span className="text-indigo-600 font-bold">Adminístralos</span>
       </p>
+
+      <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+        <div className="mb-5">
+          <label
+            htmlFor="petname"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Nombre Mascota
+          </label>
+
+          <input
+            id="petname"
+            type="text"
+            placeholder="Nombre de la Mascota"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+        </div>
+
+        <div className="mb-5">
+          <label
+            htmlFor="ownername"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Nombre Propietario
+          </label>
+
+          <input
+            id="ownername"
+            type="text"
+            placeholder="Nombre del Propietario"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+        </div>
+
+        <div className="mb-5">
+          <label
+            htmlFor="email"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Email
+          </label>
+
+          <input
+            id="email"
+            type="email"
+            placeholder="Email Contacto Propietario"
+            className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+          />
+        </div>
+
+        <div className="mb-5">
+          <label
+            htmlFor="medicalDiscarge"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Alta
+          </label>
+
+          <input
+            id="medicalDiscarge"
+            type="date"
+            className="border-2 w-full p-2 mt-2 rounded-md"
+          />
+        </div>
+
+        <div className="mb-5">
+          <label
+            htmlFor="symptoms"
+            className="block text-gray-700 uppercase font-bold"
+          >
+            Síntomas
+          </label>
+
+          <textarea
+            id="symptoms"
+            className="border-2 w-full p-2 mt-2 rounded-md"
+            placeholder="Describe los Síntomas"
+          />
+        </div>
+
+        <input
+          type="submit"
+          className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-all"
+          value="Agregar Paciente"
+        />
+      </form>
     </div>
   );
 }
