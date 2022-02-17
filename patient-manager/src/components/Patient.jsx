494a05e39caa2d5e6081e824dabc63599455f59f
inmacc96 <inmacaballerocarrero@gmail.com>
2022-02-17 12:24:17 +0100

    Added basic structure for patient appointment card

---
+++ patient-manager/src/components/Patient.jsx
@@ -0,0 +1,33 @@
+function Patient() {
+  return (
+    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Nombre: {""}
+        <span className="font-normal normal-case">Hook</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Propietario: {""}
+        <span className="font-normal normal-case">Inma</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Email: {""}
+        <span className="font-normal normal-case">correo@correo.com</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Fecha Alta: {""}
+        <span className="font-normal normal-case">10 Diciembre 2022</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Sintomas: {""}
+        <span className="font-normal normal-case">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Impedit
+          voluptatibus veniam, sint quae sapiente asperiores nemo amet commodi
+          atque, assumenda temporibus obcaecati, similique aspernatur ullam
+          perferendis doloribus numquam exercitationem minus?
+        </span>
+      </p>
+    </div>
+  );
+}
+
+export default Patient;
--- patient-manager/src/components/PatientsList.jsx
+++ patient-manager/src/components/PatientsList.jsx
@@ -1,7 +1,18 @@
+import Patient from "./Patient";
+
 function PatientsList() {
   return (
-    <div className="md:w-1/2 lg:w-3/5">
-      <h2>Listado Pacientes</h2>
+    <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
+      <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
+      <p className="text-xl mt-5 mb-10 text-center">
+        Administra tus {""}
+        <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
+      </p>
+
+      <Patient />
+      <Patient />
+      <Patient />
+      <Patient />
     </div>
   );
 }
