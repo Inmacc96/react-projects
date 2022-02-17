782b1c62c44c88290089a3356c83f31a25bde4b6
inmacc96 <inmacaballerocarrero@gmail.com>
2022-02-17 13:39:51 +0100

    Aligned form and patient appointment card.

--- patient-manager/src/components/Patient.jsx
+++ patient-manager/src/components/Patient.jsx
@@ -1,6 +1,6 @@
 function Patient() {
   return (
-    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+    <div className="mx-3 my-10 bg-white shadow-md px-5 py-10 rounded-xl">
       <p className="font-bold mb-3 text-gray-700 uppercase">
         Nombre: {""}
         <span className="font-normal normal-case">Hook</span>
