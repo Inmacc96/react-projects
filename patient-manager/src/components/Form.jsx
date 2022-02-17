47ff30087a78af199040b8e4210290d52a7beea0
inmacc96 <inmacaballerocarrero@gmail.com>
2022-02-17 13:40:50 +0100

    Read the information written on the form and save it in its corresponding status.

--- patient-manager/src/components/Form.jsx
+++ patient-manager/src/components/Form.jsx
@@ -1,6 +1,19 @@
+import { useState, useEffect } from "react";
+
 function Form() {
+  const [name, setName] = useState('');
+  const [owner, setOwner] = useState('');
+  const [email, setEmail] = useState('');
+  const [date, setDate] = useState('');
+  const [symptoms, setSymptoms] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); // Previene la opción por defecto y lo validas antes de enviar el formulario
+    console.log("Enviando Formulario");
+  };
+
   return (
-    <div className="md:w-1/2 lg:w-2/5">
+    <div className="md:w-1/2 lg:w-2/5 mx-5">
       <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
 
       <p className="text-lg mt-5 text-center mb-10">
@@ -8,7 +21,10 @@ function Form() {
         <span className="text-indigo-600 font-bold">Adminístralos</span>
       </p>
 
-      <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
+      >
         <div className="mb-5">
           <label
             htmlFor="petname"
@@ -22,6 +38,8 @@ function Form() {
             type="text"
             placeholder="Nombre de la Mascota"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
         </div>
 
@@ -38,6 +56,8 @@ function Form() {
             type="text"
             placeholder="Nombre del Propietario"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            value={owner}
+            onChange={(e) => setOwner(e.target.value)}
           />
         </div>
 
@@ -54,6 +74,8 @@ function Form() {
             type="email"
             placeholder="Email Contacto Propietario"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
 
@@ -69,6 +91,8 @@ function Form() {
             id="medicalDiscarge"
             type="date"
             className="border-2 w-full p-2 mt-2 rounded-md"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
           />
         </div>
 
@@ -84,6 +108,8 @@ function Form() {
             id="symptoms"
             className="border-2 w-full p-2 mt-2 rounded-md"
             placeholder="Describe los Síntomas"
+            value={symptoms}
+            onChange={(e) => setSymptoms(e.target.value)}
           />
         </div>
 
