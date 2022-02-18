a03fde17a8ed1330e8945477b638e5d5fecaa334
inmacc96 <inmacaballerocarrero@gmail.com>
2022-02-18 13:11:18 +0100

    Added a conditional for the patient list, separating the case when there are appointments and when there are no appointments.

--- patient-manager/src/components/PatientsList.jsx
+++ patient-manager/src/components/PatientsList.jsx
@@ -1,18 +1,31 @@
 import Patient from "./Patient";
 
 function PatientsList({ patients }) {
-  console.log(patients);
   return (
     <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
-      <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
-      <p className="text-xl mt-5 mb-10 text-center">
-        Administra tus {""}
-        <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
-      </p>
+      {patients && patients.length ? (
+        <>
+          <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Administra tus {""}
+            <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
+          </p>
 
-      {patients.map((patient) => (
-        <Patient key={patient.id} patient={patient} />
-      ))}
+          {patients.map((patient) => (
+            <Patient key={patient.id} patient={patient} />
+          ))}
+        </>
+      ) : (
+        <>
+          <h2 className="font-black text-3xl text-center">No hay pacientes</h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Comienza agregando pacientes {""}
+            <span className="text-indigo-600 font-bold">
+              y aparecerán en este lugar{" "}
+            </span>
+          </p>
+        </>
+      )}
     </div>
   );
 }
