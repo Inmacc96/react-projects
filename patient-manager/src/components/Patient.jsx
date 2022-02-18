4ce15b7daf62afb93b7472068873d7e5f7956cca
inmacc96 <inmacaballerocarrero@gmail.com>
2022-02-18 13:17:34 +0100

    Added buttons to edit and delete appointments.

--- patient-manager/src/components/Patient.jsx
+++ patient-manager/src/components/Patient.jsx
@@ -22,6 +22,21 @@ function Patient({ patient }) {
         Sintomas: {""}
         <span className="font-normal normal-case">{symptoms}</span>
       </p>
+
+      <div className="flex justify-between mt-10">
+        <button
+          type="button"
+          className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+        >
+          Editar
+        </button>
+        <button
+          type="button"
+          className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
+        >
+          Eliminar
+        </button>
+      </div>
     </div>
   );
 }
