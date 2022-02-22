fc0cfb1b4b1d0cb5ad1754c58d1737d061665198
inmacc96 <inmacaballerocarrero@gmail.com>
2022-02-22 11:02:40 +0100

    Added formatAmount function.

--- expenditure-control/src/components/BudgetControl.jsx
+++ expenditure-control/src/components/BudgetControl.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 
-const BudgetControl = ({budget}) => {
+const BudgetControl = ({ budget }) => {
+  const formatAmount = (amount) => {
+    return amount.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+  };
   return (
     <div className="contenedor-presupuesto contendor sombra dos-columnas">
       <div>
@@ -9,7 +15,15 @@ const BudgetControl = ({budget}) => {
 
       <div className="contenido-presupuesto">
         <p>
-          <span>Presupuesto: </span> ${budget}
+          <span>Presupuesto: </span> {formatAmount(budget)}
+        </p>
+
+        <p>
+          <span>Disponible: </span> {formatAmount(0)}
+        </p>
+
+        <p>
+          <span>Gastado: </span> {formatAmount(0)}
         </p>
       </div>
     </div>
