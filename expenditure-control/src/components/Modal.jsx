a021112d24b912daaa3f9de7f064298c8bcd7537
inmacc96 <inmacaballerocarrero@gmail.com>
2022-02-23 10:00:32 +0100

    Added states for modal form inputs.

--- expenditure-control/src/components/Modal.jsx
+++ expenditure-control/src/components/Modal.jsx
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import CloseBtn from "../img/cerrar.svg";
 
 const Modal = ({ setModal, animateModal, setanimateModal }) => {
+  const [name, setName] = useState("");
+  const [amount, setAmount] = useState(0);
+  const [category, setCategory] = useState("");
+
   const hideModal = () => {
     setanimateModal(false);
 
@@ -24,6 +29,8 @@ const Modal = ({ setModal, animateModal, setanimateModal }) => {
             id="name"
             type="text"
             placeholder="Añade el nombre del gasto"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
         </div>
 
@@ -34,13 +41,19 @@ const Modal = ({ setModal, animateModal, setanimateModal }) => {
             id="amount"
             type="number"
             placeholder="Añade la cantidad del gasto: ej. 300"
+            value={amount}
+            onChange={(e) => setAmount(Number(e.target.value))}
           />
         </div>
 
         <div className="campo">
           <label htmlFor="category">Categoría</label>
 
-          <select id="category">
+          <select
+            id="category"
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+          >
             <option value="">-- Seleccione --</option>
             <option value="saving">Ahorro</option>
             <option value="food">Comida</option>
