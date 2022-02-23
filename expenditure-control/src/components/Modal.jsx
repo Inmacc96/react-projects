26cbd90e7480c89fe645b58c3d1f8221eb6aabb7
inmacc96 <inmacaballerocarrero@gmail.com>
2022-02-23 09:44:08 +0100

    Added the expenditure form in the modal.

--- expenditure-control/src/components/Modal.jsx
+++ expenditure-control/src/components/Modal.jsx
@@ -16,6 +16,43 @@ const Modal = ({ setModal, animateModal, setanimateModal }) => {
 
       <form className={`formulario ${animateModal ? "animar" : "cerrar"}`}>
         <legend>Nuevo Gasto</legend>
+
+        <div className="campo">
+          <label htmlFor="name">Nombre Gasto</label>
+
+          <input
+            id="name"
+            type="text"
+            placeholder="Añade el nombre del gasto"
+          />
+        </div>
+
+        <div className="campo">
+          <label htmlFor="amount">Cantidad</label>
+
+          <input
+            id="amount"
+            type="number"
+            placeholder="Añade la cantidad del gasto: ej. 300"
+          />
+        </div>
+
+        <div className="campo">
+          <label htmlFor="category">Categoría</label>
+
+          <select id="category">
+            <option value="">-- Seleccione --</option>
+            <option value="saving">Ahorro</option>
+            <option value="food">Comida</option>
+            <option value="house">Casa</option>
+            <option value="miscellaneous">Gastos Varios</option>
+            <option value="leisure">Ocio</option>
+            <option value="health">Salud</option>
+            <option value="subscription">Suscripciones</option>
+          </select>
+        </div>
+
+        <input type="submit" value="Añadir Gasto" />
       </form>
     </div>
   );
