582fbc2ca7d0255b03e6652a6b02f08e296cd707
inmacc96 <inmacaballerocarrero@gmail.com>
2022-02-24 09:28:59 +0100

    Added expenses icon.

--- expenditure-control/src/components/Expenditure.jsx
+++ expenditure-control/src/components/Expenditure.jsx
@@ -1,11 +1,30 @@
 import { formatDate } from "../helpers";
 
+import savingIcon from "../img/icono_ahorro.svg";
+import houseIcon from "../img/icono_casa.svg";
+import foodIcon from "../img/icono_comida.svg";
+import miscellaneousExpensesIcon from "../img/icono_gastos.svg";
+import leisureIcon from "../img/icono_ocio.svg";
+import healthIcon from "../img/icono_salud.svg";
+import subscriptionIcon from "../img/icono_suscripciones.svg";
+
+const icons = {
+  ahorro: savingIcon,
+  house: houseIcon,
+  comida: foodIcon,
+  gastos: miscellaneousExpensesIcon,
+  ocio: leisureIcon,
+  salud: healthIcon,
+  suscripciones: subscriptionIcon,
+};
+
 const Expenditure = ({ expenditure }) => {
   const { category, name, amount, id, date } = expenditure;
+
   return (
     <div className="gasto sombra">
       <div className="contenido-gasto">
-        {/* Imagen */}
+        <img src={icons[category]} alt="Icono gasto" />
         <div className="descripcion-gasto">
           <p className="categoria">{category}</p>
           <p className="nombre-gasto">{name}</p>
