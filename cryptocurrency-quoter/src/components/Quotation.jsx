43ddb82986e8410c853980303fd23fa197ed45b5
inmacc96 <inmacaballerocarrero@gmail.com>
2022-03-01 19:12:02 +0100

    Formated the quotation result.

--- cryptocurrency-quoter/src/components/Quotation.jsx
+++ cryptocurrency-quoter/src/components/Quotation.jsx
@@ -1,16 +1,61 @@
 import styled from "@emotion/styled";
 
+const Container = styled.div`
+  color: #fff;
+  font-family: "Lato", sans-serif;
+
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  margin-top: 30px;
+`;
+
+const Image = styled.img`
+  display: block;
+  width: 120px;
+`;
+
+const Text = styled.p`
+  font-size: 18px;
+  span {
+    font-weight: 700;
+  }
+`;
+
+const Price = styled.p`
+  font-size: 25px;
+  span {
+    font-weight: 700;
+  }
+`;
+
 const Quotation = ({ quotation }) => {
-  const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEUR, LASTUPDATE } =
+  const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE } =
     quotation;
   return (
-    <div>
-      <p>El Precio es de: <span>{PRICE}</span></p>
-      <p>Precio más alto del día: <span>{HIGHDAY}</span></p>
-      <p>Precio más bajo del día: <span>{LOWDAY}</span></p>
-      <p>Variación últimas 24 horas: <span>{CHANGEPCT24HOUR}</span></p>
-      <p>Última actualización: <span>{LASTUPDATE}</span></p>
-    </div>
+    <Container>
+      <Image
+        src={`https://cryptocompare.com/${IMAGEURL}`}
+        alt="cryptocurrency image"
+      />
+      <div>
+        <Price>
+          El Precio es de: <span>{PRICE}</span>
+        </Price>
+        <Text>
+          Precio más alto del día: <span>{HIGHDAY}</span>
+        </Text>
+        <Text>
+          Precio más bajo del día: <span>{LOWDAY}</span>
+        </Text>
+        <Text>
+          Variación últimas 24 horas: <span>{CHANGEPCT24HOUR}</span>
+        </Text>
+        <Text>
+          Última actualización: <span>{LASTUPDATE}</span>
+        </Text>
+      </div>
+    </Container>
   );
 };
 
