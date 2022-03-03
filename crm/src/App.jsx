3e7b463f89ab5b93291b55691d46709278aa959c
inmacc96 <inmacaballerocarrero@gmail.com>
2022-03-03 10:55:15 +0100

    Grouping routes and defining main layout.

--- crm/src/App.jsx
+++ crm/src/App.jsx
@@ -1,20 +1,18 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
-import LogIn from "./layout/LogIn";
 import Layout from "./layout/Layout";
 import Home from "./pages/Home";
-import LoginForm from "./pages/LoginForm";
+import NewCustomer from "./pages/NewCustomer";
+import EditCustomer from "./pages/EditCustomer";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-          <Route path="/" element={<LogIn />}>
-            <Route index element={<LoginForm/>}/>
-          </Route>
-
           <Route path="/customers" element={<Layout />}>
             <Route index element={<Home />}/>
+            <Route path="new" element={<NewCustomer />}/>
+            <Route path="edit/:id" element={<EditCustomer />}/>
           </Route>
       </Routes>
     </BrowserRouter>
