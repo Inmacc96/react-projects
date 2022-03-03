b924e861ecf5b6dca55a58365b882852b618ac50
inmacc96 <inmacaballerocarrero@gmail.com>
2022-03-03 11:16:53 +0100

    Highlighted the section of the current page using the useLocation hook.

--- crm/src/layout/Layout.jsx
+++ crm/src/layout/Layout.jsx
@@ -1,6 +1,9 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useLocation } from "react-router-dom";
 
 const Layout = () => {
+  const location = useLocation();
+  const currentUrl = location.pathname;
+
   return (
     <div className="md:flex md:min-h-screen">
       <div className="md:w-1/4 bg-blue-900 px-5 py-10">
@@ -10,13 +13,17 @@ const Layout = () => {
 
         <nav className="mt-10">
           <Link
-            className="text-white text-2xl block mt-2 hover:text-blue-300"
+            className={`${
+              currentUrl == "/customers" ? "text-blue-300" : "text-white"
+            } text-2xl block mt-2 hover:text-blue-300`}
             to="/customers"
           >
             Clientes
           </Link>
           <Link
-            className="text-white text-2xl block mt-2 hover:text-blue-300"
+            className={`${
+              currentUrl == "/customers/new" ? "text-blue-300" : "text-white"
+            } text-2xl block mt-2 hover:text-blue-300`}
             to="/customers/new"
           >
             Nuevo Cliente
