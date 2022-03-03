053147f88afe6a02ac694cd3b7d177ab9d619eae
inmacc96 <inmacaballerocarrero@gmail.com>
2022-03-03 11:03:52 +0100

    Applying Tailwind CSS to the main layout.

--- crm/src/layout/Layout.jsx
+++ crm/src/layout/Layout.jsx
@@ -2,10 +2,31 @@ import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <div>
-      <h1>Desde Layout.jsx</h1>
+    <div className="md:flex md:min-h-screen">
+      <div className="md:w-1/4 bg-blue-900 px-5 py-10">
+        <h2 className="text-4xl font-black text-center text-white">
+          CRM - Clientes
+        </h2>
 
-      <Outlet />
+        <nav className="mt-10">
+          <a
+            className="text-white text-2xl block mt-2 hover:text-blue-300"
+            href="/customers"
+          >
+            Clientes
+          </a>
+          <a
+            className="text-white text-2xl block mt-2 hover:text-blue-300"
+            href="/customers/new"
+          >
+            Nuevo Cliente
+          </a>
+        </nav>
+      </div>
+
+      <div className="md:w-3/4">
+        <Outlet />
+      </div>
     </div>
   );
 };
