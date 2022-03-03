f6b5b00da317d6f78edff1ccddaadc6f84cdadf0
inmacc96 <inmacaballerocarrero@gmail.com>
2022-03-03 11:50:49 +0100

    Added customer form with Formik dependency.

--- crm/src/pages/NewCustomer.jsx
+++ crm/src/pages/NewCustomer.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Form from '../components/Form'
+import CustomerForm from '../components/CustomerForm'
 
 const NewCustomer = () => {
   return (
     <>
       <h1 className="font-black text-4xl text-blue-900">Nuevo Cliente</h1>
       <p className="mt-3">Llena los siguientes campos para registrar un cliente</p>
-      <Form />
+      <CustomerForm />
     </>
   )
 }
