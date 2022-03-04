92017a1e3f0d6c98bcb6c3e62625223d8122dd0a
inmacc96 <inmacaballerocarrero@gmail.com>
2022-03-04 14:12:10 +0100

    Showed customer information in the table.

--- crm/src/components/Customer.jsx
+++ crm/src/components/Customer.jsx
@@ -1,10 +1,44 @@
 import React from "react";
 
 const Customer = ({ customer }) => {
+  const { customerName, email, phone, company } = customer;
   return (
-    <div>
-      <h1>{customer.customerName}</h1>
-    </div>
+    <tr className="border-b hover:bg-gray-50">
+      <td className="p-3">{customerName}</td>
+      <td className="p-3">
+        <p>
+          {" "}
+          <span className="text-gray-800 uppercase font-bold">Email:</span>{" "}
+          {email}
+        </p>
+        <p>
+          {" "}
+          <span className="text-gray-800 uppercase font-bold">Tel:</span>{" "}
+          {phone}
+        </p>
+      </td>
+      <td className="p-3">{company}</td>
+      <td className="p-3">
+        <button
+          type="button"
+          className="bg-yellow-500 hover:bg-yellow-600 w-full text-white p-2 uppercase font-bold text-xs mt-3"
+        >
+          Ver
+        </button>
+        <button
+          type="button"
+          className="bg-blue-600 hover:bg-blue-700 w-full text-white p-2 uppercase font-bold text-xs mt-3"
+        >
+          Editar
+        </button>
+        <button
+          type="button"
+          className="bg-red-600 hover:bg-red-700 w-full text-white p-2 uppercase font-bold text-xs mt-3"
+        >
+          Eliminar
+        </button>
+      </td>
+    </tr>
   );
 };
 
