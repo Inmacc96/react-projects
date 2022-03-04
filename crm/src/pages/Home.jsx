1e8919d75e3a3cb106268a1663fd75436d120b2d
inmacc96 <inmacaballerocarrero@gmail.com>
2022-03-04 13:49:55 +0100

    Obtained clients by querying the API.

--- crm/src/pages/Home.jsx
+++ crm/src/pages/Home.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { useState, useEffect } from "react";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [customers, setCustomers] = useState([]);
 
-export default Home
+  //Este es un excelente uso del useEffect: cuando quieres consultar
+  // una API una vez que el componente esté listo.
+  useEffect(() => {
+    const getCustomersAPI = async () => {
+      try {
+        const url = "http://localhost:4000/customers";
+
+        const response = await fetch(url);
+
+        const results = await response.json();
+
+        setCustomers(results);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    getCustomersAPI();
+  }, []);
+
+  return <div>Home</div>;
+};
+
+export default Home;
