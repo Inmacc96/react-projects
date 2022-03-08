cff7b680ffcfc635204ff1d554cec479b9067035
inmacc96 <inmacaballerocarrero@gmail.com>
2022-03-08 10:24:11 +0100

    Studying routing in Next.js

---
+++ guitarla/pages/aboutus.js
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+const AboutUs = () => {
+  return (
+    <div>
+      <h1>Desde nosotros</h1>
+
+      <Link href="/"> Ir a inicio</Link>
+    </div>
+  );
+};
+
+export default AboutUs;
