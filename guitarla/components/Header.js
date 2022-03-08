b1b0d449dd25450fd6a2b86bf82b0f788c95252f
inmacc96 <inmacaballerocarrero@gmail.com>
2022-03-08 11:33:17 +0100

    Added logo and styles to header.

--- guitarla/components/Header.js
+++ guitarla/components/Header.js
@@ -1,22 +1,31 @@
-import Link from 'next/link';
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../styles/Header.module.css";
 
 const Header = () => {
   return (
-    <header>
-        <div className="contenedor">
-            <div>
+    <header className={styles.header}>
+      <div className="contenedor">
+        <div>
+          <Link href="/">
+            <Image
+              width={400}
+              height={100}
+              src="/img/logo.svg"
+              alt="Imagen logo"
+            />
+          </Link>
+        </div>
 
-            </div>
-
-            <nav>
-                <Link href="/">Inicio</Link>
-                <Link href="/aboutus">Nosotros</Link>
-                <Link href="/blog">Blog</Link>
-                <Link href="/store">Tienda</Link>
-            </nav>
-            </div>
+        <nav>
+          <Link href="/">Inicio</Link>
+          <Link href="/aboutus">Nosotros</Link>
+          <Link href="/blog">Blog</Link>
+          <Link href="/store">Tienda</Link>
+        </nav>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
