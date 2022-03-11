dce5d1346373e422e762b6a893e453f2ce0acece
inmacc96 <inmacaballerocarrero@gmail.com>
2022-03-11 12:17:38 +0100

    Querying the strapi API with getServerSideProps.

--- guitarla/pages/blog.js
+++ guitarla/pages/blog.js
@@ -1,11 +1,23 @@
 import Layout from "../components/Layout";
 
-const Blog = () => {
+const Blog = ({result}) => {
+  console.log(result)
   return (
     <Layout page="Blog">
       <h1>Desde Blog</h1>
     </Layout>
   );
 };
+
+export async function getServerSideProps() {
+  const url = "http://localhost:1337/blogs";
+  const response = await fetch(url);
+  const result = await response.json();
+  return {
+    props: {
+      result
+    },
+  };
+}
 
 export default Blog;
