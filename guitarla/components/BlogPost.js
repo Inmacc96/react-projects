d70edf2936e2b0cf2cb2a347b86264afa4318649
inmacc96 <inmacaballerocarrero@gmail.com>
2022-03-14 15:56:20 +0100

    Added the images for the blog posts.

--- guitarla/components/BlogPost.js
+++ guitarla/components/BlogPost.js
@@ -1,17 +1,17 @@
 import Link from "next/link";
+import Image from "next/image";
 import { formatDate } from "../helpers"; // Al llamarse el archivo index.js, solo es suficiente con el nombre de la carpeta
 
 const BlogPost = ({ blogpost }) => {
   const { title, summary, image, published_at, id } = blogpost;
   return (
     <article>
+        <Image layout='responsive' width={800} height={600} src={image.url} alt={`imagen blog${title}`}/>
       <div>
         <h1>{title}</h1>
         <p>{formatDate(published_at)}</p>
         <p>{summary}</p>
-        <Link href={`/blog/${id}`}>
-            Leer Entrada
-        </Link>
+        <Link href={`/blog/${id}`}>Leer Entrada</Link>
       </div>
     </article>
   );
