951e03cb78b592cae940a6e17eff060f9533cd32
inmacc96 <inmacaballerocarrero@gmail.com>
2022-03-14 15:47:34 +0100

    Showing the title, summary, date and button of each blog post.

--- guitarla/components/BlogPost.js
+++ guitarla/components/BlogPost.js
@@ -1,11 +1,20 @@
+import Link from "next/link";
+import { formatDate } from "../helpers"; // Al llamarse el archivo index.js, solo es suficiente con el nombre de la carpeta
 
-const BlogPost = ({blogpost}) => {
-    const {title, summary, image, published_at, id} = blogpost
+const BlogPost = ({ blogpost }) => {
+  const { title, summary, image, published_at, id } = blogpost;
   return (
     <article>
+      <div>
         <h1>{title}</h1>
+        <p>{formatDate(published_at)}</p>
+        <p>{summary}</p>
+        <Link href={`/blog/${id}`}>
+            Leer Entrada
+        </Link>
+      </div>
     </article>
-  )
-}
+  );
+};
 
-export default BlogPost
+export default BlogPost;
---
+++ guitarla/helpers/index.js
@@ -0,0 +1,11 @@
+export const formatDate = (date) => {
+  const newDate = new Date(date);
+
+  const options = {
+    year: "numeric",
+    month: "long",
+    day: "2-digit",
+  };
+
+  return newDate.toLocaleDateString("es-ES", options);
+};
