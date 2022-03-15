f4e817e7a94e627ff2a3bcb285470fe730c66335
inmacc96 <inmacaballerocarrero@gmail.com>
2022-03-15 13:46:31 +0100

    Added css to guitar catalogue.

--- guitarla/components/Guitar.js
+++ guitarla/components/Guitar.js
@@ -1,24 +1,25 @@
 import Image from "next/image";
 import Link from "next/link";
+import styles from "../styles/Guitar.module.css";
 
 const Guitar = ({ guitar }) => {
   const { description, url, image, name, price } = guitar;
   return (
-    <div>
+    <div className={styles.guitar}>
       <Image
         layout="responsive"
-        width={500}
+        width={180}
         height={350}
         src={image.url}
         alt={`Imagen Guitarra ${name}`}
       />
-      <div>
-          <h3>{name}</h3>
-          <p>{description}</p>
-          <p>{price}</p>
-          <Link href={`/guitars/${url}`}>
-              Ver Producto
-          </Link>
+      <div className={styles.container}>
+        <h3>{name}</h3>
+        <p className={styles.description}>{description}</p>
+        <p className={styles.price}>${price}</p>
+        <Link href={`/guitars/${url}`}>
+          <a className={styles.link}>Ver Producto</a>
+        </Link>
       </div>
     </div>
   );
--- guitarla/components/List.js
+++ guitarla/components/List.js
@@ -1,8 +1,9 @@
 import Guitar from "./Guitar";
+import styles from "../styles/List.module.css"
 
 const List = ({ guitars }) => {
   return (
-    <div>
+    <div className={styles.list}>
       {guitars.map((guitar) => (
         <Guitar key={guitar.id} guitar={guitar} />
       ))}
