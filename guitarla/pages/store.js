60f5779d62891e07d9e8bfd2333f1c26c5ec99e3
inmacc96 <inmacaballerocarrero@gmail.com>
2022-03-15 13:04:56 +0100

    Querying the API for guitars.

--- guitarla/pages/store.js
+++ guitarla/pages/store.js
@@ -1,11 +1,24 @@
 import Layout from "../components/Layout";
 
-const Store = () => {
+const Store = ({ guitars }) => {
   return (
     <Layout page="Tienda">
-      <h1>Desde Tienda</h1>
+      <main className="contenedor">
+        <h1 className="heading">Nuestra Colección</h1>
+      </main>
     </Layout>
   );
 };
+
+export async function getServerSideProps() {
+  const url = `${process.env.API_URL}/guitars`;
+  const response = await fetch(url);
+  const guitars = await response.json();
+  return {
+    props: {
+      guitars,
+    },
+  };
+}
 
 export default Store;
