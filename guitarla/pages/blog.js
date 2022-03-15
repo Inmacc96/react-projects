94446b9f215769e9e34a6530a7432ff42d5a7167
inmacc96 <inmacaballerocarrero@gmail.com>
2022-03-15 13:46:58 +0100

    Ordered the blogs from most recent to oldest.

--- guitarla/pages/blog.js
+++ guitarla/pages/blog.js
@@ -19,7 +19,7 @@ const Blog = ({ blogposts }) => {
 };
 
 export async function getServerSideProps() {
-  const url = `${process.env.API_URL}/blogs`;
+  const url = `${process.env.API_URL}/blogs/?_sort=createdAt:desc`;
   const response = await fetch(url);
   const blogposts = await response.json();
   return {
