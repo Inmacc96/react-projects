9c304733777d2d023902260953ee6d313863e8e8
inmacc96 <inmacaballerocarrero@gmail.com>
2022-03-15 14:07:28 +0100

    Added Outfit custom font.

--- guitarla/components/Layout.js
+++ guitarla/components/Layout.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Header from './Header';
+import Header from "./Header";
 import Footer from "./Footer";
 
 const Layout = ({ children, page }) => {
@@ -8,11 +8,17 @@ const Layout = ({ children, page }) => {
       <Head>
         <title>GuitarLA - {page}</title>
         <meta name="description" content="Sitio Web de venta de guitarras" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;700;900&display=swap"
+          rel="stylesheet"
+        />
       </Head>
 
       <Header />
       {children}
-      <Footer/>
+      <Footer />
     </div>
   );
 };
