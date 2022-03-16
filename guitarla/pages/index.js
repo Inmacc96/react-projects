49ab9160aea2ade3a614c45e564fcb32edfec422
inmacc96 <inmacaballerocarrero@gmail.com>
2022-03-16 16:04:17 +0100

    Added the guitar catalogue to the home page.

--- guitarla/pages/index.js
+++ guitarla/pages/index.js
@@ -1,9 +1,27 @@
 import Layout from "../components/Layout";
+import List from "../components/List";
 
-export default function Home() {
+const Home = ({ guitars }) => {
   return (
     <Layout page="Inicio">
-      <h1>Desde Inicio</h1>
+      <main className="contenedor">
+        <h1 className="heading">Nuestra Colección</h1>
+        <List guitars={guitars} />
+      </main>
     </Layout>
   );
+};
+
+export async function getServerSideProps() {
+  const url = `${process.env.API_URL}/guitars`;
+  const response = await fetch(url);
+  const guitars = await response.json();
+
+  return {
+    props: {
+      guitars,
+    },
+  };
 }
+
+export default Home;
