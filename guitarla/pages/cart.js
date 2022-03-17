f681ad6a28618e27e6d30126b8c8c9bde674e94b
inmacc96 <inmacaballerocarrero@gmail.com>
2022-03-17 13:37:54 +0100

    Adding the selected guitars to the cart page.

--- guitarla/pages/cart.js
+++ guitarla/pages/cart.js
@@ -1,12 +1,41 @@
 import Layout from "../components/Layout";
+import Image from "next/image";
 import styles from "../styles/Cart.module.css";
 
-const Cart = () => {
+const Cart = ({ cart }) => {
+  console.log(cart);
   return (
     <Layout page="Carrito de compras">
       <h1 className="heading">Carrito</h1>
       <main className={`contenedor ${styles.content}`}>
-        <div>1</div>
+        <div className={styles.cart}>
+          {cart.length === 0
+            ? "Carrito Vacío"
+            : cart.map((product) => (
+                <div key={product.id} className={styles.product}>
+                  <div>
+                    <Image
+                      layout="responsive"
+                      width={250}
+                      height={480}
+                      src={product.image}
+                      alt={product.name}
+                    />
+                  </div>
+
+                  <div>
+                    <p className={styles.name}>{product.name}</p>
+                    <p className={styles.amount}>Cantidad: {product.amount}</p>
+                    <p className={styles.price}>
+                      $<span>{product.price}</span>
+                    </p>
+                    <p className={styles.subtotal}>
+                      Subtotal: $<span>{product.price * product.amount}</span>
+                    </p>
+                  </div>
+                </div>
+              ))}
+        </div>
         <div>2</div>
       </main>
     </Layout>
