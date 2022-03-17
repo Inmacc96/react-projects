d49347d6d45c94c4aa12ed658b7fb344398269a1
inmacc96 <inmacaballerocarrero@gmail.com>
2022-03-17 12:49:52 +0100

    Avoiding duplication in the cart.

--- guitarla/pages/_app.js
+++ guitarla/pages/_app.js
@@ -6,7 +6,17 @@ function MyApp({ Component, pageProps }) {
   const [cart, setCart] = useState([]);
 
   const addToCart = (product) => {
-    setCart([...cart, product]);
+    if (cart.some((article) => article.id === product.id)) {
+      const updateCart = cart.map((article) => {
+        if (article.id === product.id) {
+          article.amount = product.amount;
+        }
+        return article;
+      });
+      setCart(updateCart);
+    } else {
+      setCart([...cart, product]);
+    }
   };
   return <Component {...pageProps} cart={cart} addToCart={addToCart} />;
 }
