1042a918fa83b53ba370e94c7dff0f084d5c39dd
inmacc96 <inmacaballerocarrero@gmail.com>
2022-03-22 19:30:49 +0100

    Delete value srv in database.js

--- guitarLA_strapi/config/database.js
+++ guitarLA_strapi/config/database.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
       connector: "mongoose",
       settings: {
         host: env("DATABASE_HOST"),
-        srv: env.bool("DATABASE_SRV", true),
+        srv: env.bool("DATABASE_SRV"),
         port: env.int("DATABASE_PORT"),
         database: env("DATABASE_NAME"),
         username: env("DATABASE_USERNAME"),
