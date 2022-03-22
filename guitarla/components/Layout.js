647863a1d33fea800ab8d58c7805da7e58aac8ea
inmacc96 <inmacaballerocarrero@gmail.com>
2022-03-22 20:01:32 +0100

    Add font url to globals.css

--- guitarla/components/Layout.js
+++ guitarla/components/Layout.js
@@ -8,12 +8,6 @@ const Layout = ({ children, page, guitar }) => {
       <Head>
         <title>GuitarLA - {page}</title>
         <meta name="description" content="Sitio Web de venta de guitarras" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;700;900&display=swap"
-          rel="stylesheet"
-        />
       </Head>
 
       <Header guitar={guitar}/>
