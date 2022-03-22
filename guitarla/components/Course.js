e287e9d70956598ce9c63a7e20f461a47a96e425
inmacc96 <inmacaballerocarrero@gmail.com>
2022-03-22 20:24:55 +0100

    import Link component.

--- guitarla/components/Course.js
+++ guitarla/components/Course.js
@@ -1,5 +1,5 @@
 import styles from "../styles/Course.module.css";
-import Link from "next/Link";
+import Link from "next/link";
 
 const Course = ({ course }) => {
   const { title, content, image } = course;
