de2a28c4cf1fa96f7edc08f379dad0240e7a6067
inmacc96 <inmacaballerocarrero@gmail.com>
2022-03-24 20:50:45 +0100

    Add summary information to the result component.

--- quoter/src/components/Result.jsx
+++ quoter/src/components/Result.jsx
@@ -1,9 +1,40 @@
 import useQuoter from "../hooks/useQuoter";
+import { brands, plans } from "../constants";
 
 const Result = () => {
-  const { result } = useQuoter();
+  const { result, data } = useQuoter();
+  const { brand, plan, year } = data;
+
+  const [nameBrand] = brands.filter((b) => b.id === Number(brand));
+  const [namePlan] = plans.filter((p) => p.id == Number(plan));
+
   if (result === 0) return null;
-  return <div> {result} </div>;
+
+  return (
+    <div className="bg-gray-100 text-center mt-5 p-5 shadow">
+      <h2 className="text-gray-600 font-black text-3xl">Resumen</h2>
+
+      <p className="my-2">
+        <span className="font-bold">Marca: </span>
+        {nameBrand.name}
+      </p>
+
+      <p className="my-2">
+        <span className="font-bold">Plan: </span>
+        {namePlan.name}
+      </p>
+
+      <p className="my-2">
+        <span className="font-bold">Año del Auto: </span>
+        {year}
+      </p>
+
+      <p className="my-2 text-2xl">
+        <span className="font-bold">Total Cotización: </span>
+        {result}
+      </p>
+    </div>
+  );
 };
 
 export default Result;
