479ce37d6a09cc7cf6861e9c04dfaa31f90768e3
inmacc96 <inmacaballerocarrero@gmail.com>
2022-03-25 18:51:53 +0100

    Add search form .

--- lyrics-music/src/components/Form.jsx
+++ lyrics-music/src/components/Form.jsx
@@ -1,9 +1,45 @@
-    import React from 'react'
-    
-    const Form = () => {
-      return (
-        <div>Form</div>
-      )
-    }
-    
-    export default Form
+import { useState } from "react";
+
+const Form = () => {
+  const [search, setSearch] = useState({
+    artist: "",
+    song: "",
+  });
+
+  return (
+    <form>
+      <legend>Busca por Artistas y Canción</legend>
+
+      <div className="form-grid">
+        <div>
+          <label>Artista</label>
+          <input
+            type="text"
+            name="artist"
+            placeholder="Nombre Artista"
+            value={search.artist}
+            onChange={(e) =>
+              setSearch({ ...search, [e.target.name]: e.target.value })
+            }
+          />
+        </div>
+
+        <div>
+          <label>Canción</label>
+          <input
+            type="text"
+            name="song"
+            placeholder="Nombre Canción"
+            value={search.song}
+            onChange={(e) =>
+              setSearch({ ...search, [e.target.name]: e.target.value })
+            }
+          />
+        </div>
+        <input type="submit" value="Buscar" />
+      </div>
+    </form>
+  );
+};
+
+export default Form;
