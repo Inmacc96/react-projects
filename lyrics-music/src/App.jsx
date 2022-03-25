f8d59beaec1383aa6459a54bde2712b4e3bcca3a
inmacc96 <inmacaballerocarrero@gmail.com>
2022-03-25 18:17:15 +0100

    Create lyrics music app.

---
+++ lyrics-music/src/App.jsx
@@ -0,0 +1,7 @@
+import LyricsApp from "./components/LyricsApp";
+
+function App() {
+  return <LyricsApp />;
+}
+
+export default App;
---
+++ lyrics-music/src/components/LyricsApp.jsx
@@ -0,0 +1,15 @@
+import Form from "./Form";
+
+const LyricsApp = () => {
+  return (
+    <>
+      <header>Búsqueda de Letras de Canciones</header>
+
+      <Form />
+
+      <main></main>
+    </>
+  );
+};
+
+export default LyricsApp;
