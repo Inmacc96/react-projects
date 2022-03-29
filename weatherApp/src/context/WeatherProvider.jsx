10bfea7a861de3ef5a52212acc386794ed715fc2
inmacc96 <inmacaballerocarrero@gmail.com>
2022-03-29 19:01:25 +0200

    Consult the weather in the API.

--- weatherApp/src/context/WeatherProvider.jsx
+++ weatherApp/src/context/WeatherProvider.jsx
@@ -1,4 +1,5 @@
 import { useState, createContext } from "react";
+import axios from "axios";
 
 const WeatherContext = createContext();
 
@@ -7,17 +8,37 @@ const WeatherProvider = ({ children }) => {
     city: "",
     country: "",
   });
+  const [result, setResult] = useState({});
 
   const dataSearch = (e) => {
     setSearch({ ...search, [e.target.name]: e.target.value });
   };
 
-  const consultWeather = data => {
-      console.log(data)
-  }
+  const consultWeather = async (data) => {
+    try {
+      const { city, country } = data;
+
+      const appId = import.meta.env.VITE_API_KEY;
+
+      const url = `http://api.openweathermap.org/geo/1.0/direct?q=${city},${country}&limit=1&appid=${appId}`;
+
+      const { data: latlon } = await axios(url);
+      const { lat, lon } = latlon[0];
+
+      const urlWeather = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`;
+
+      const { data: weather } = await axios(urlWeather);
+
+      setResult(weather);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
-    <WeatherContext.Provider value={{ search, dataSearch, consultWeather }}>
+    <WeatherContext.Provider
+      value={{ search, dataSearch, consultWeather, result }}
+    >
       {children}
     </WeatherContext.Provider>
   );
