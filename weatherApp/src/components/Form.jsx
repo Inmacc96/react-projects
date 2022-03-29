24a5b0a153206703e6f41ac0bd6b39595bdfa33f
inmacc96 <inmacaballerocarrero@gmail.com>
2022-03-29 18:01:25 +0200

    Add the form to enter the city and country to find out its climate.

--- weatherApp/src/components/Form.jsx
+++ weatherApp/src/components/Form.jsx
@@ -1,8 +1,31 @@
-
 const Form = () => {
   return (
-    <div>Form</div>
-  )
-}
+    <div className="container">
+      <form>
+        <div className="field">
+          <label htmlFor="city">Ciudad</label>
+          <input type="text" id="city" name="city" />
+        </div>
+        <div className="field">
+          <label htmlFor="country">País</label>
+          <select id="country" name="country">
+            <option value="">Seleccione un país</option>
+            <option value="US">Estados Unidos</option>
+            <option value="ES">España</option>
+            <option value="MX">México</option>
+            <option value="AR">Argentina</option>
+            <option value="CR">Costa Rica</option>
+            <option value="FR">Francia</option>
+            <option value="IT">Italia</option>
+            <option value="GB">Reino Unido</option>
+            <option value="DE">Alemania</option>
+          </select>
+        </div>
 
-export default Form
+        <input type="submit" value="consultar clima" />
+      </form>
+    </div>
+  );
+};
+
+export default Form;
