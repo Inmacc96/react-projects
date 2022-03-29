20f0a08ca33a0a7c621afe04896d05a6c41493f9
inmacc96 <inmacaballerocarrero@gmail.com>
2022-03-29 18:19:13 +0200

    Add search state for save the city and country. And create dataSearch function for update search state.

--- weatherApp/src/context/WeatherProvider.jsx
+++ weatherApp/src/context/WeatherProvider.jsx
@@ -3,10 +3,17 @@ import { useState, createContext } from "react";
 const WeatherContext = createContext();
 
 const WeatherProvider = ({ children }) => {
+  const [search, setSearch] = useState({
+    city: "",
+    country: "",
+  });
 
+  const dataSearch = (e) => {
+    setSearch({ ...search, [e.target.name]: e.target.name });
+  };
 
   return (
-    <WeatherContext.Provider value={{ }}>
+    <WeatherContext.Provider value={{ search, dataSearch }}>
       {children}
     </WeatherContext.Provider>
   );
