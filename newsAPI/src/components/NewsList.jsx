6907c4a58c681da7486a5d6506a46dbe3bdb742b
inmacc96 <inmacaballerocarrero@gmail.com>
2022-04-02 14:33:53 +0200

    Add pagination.

--- newsAPI/src/components/NewsList.jsx
+++ newsAPI/src/components/NewsList.jsx
@@ -1,10 +1,14 @@
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
 import useNews from "../hooks/useNews";
 import News from "./News";
 
 const NewsList = () => {
-  const { news } = useNews();
+  const { news, totalNews, page, handleChangePage } = useNews();
+
+  const totalPages = Math.ceil(totalNews / 20); // 20 noticias por páginas
 
   return (
     <>
@@ -22,6 +26,23 @@ const NewsList = () => {
           <News key={item.url} news={item} />
         ))}
       </Grid>
+
+      <Stack
+        sx={{
+          marginY: 5,
+        }}
+        spacing={2}
+        direction={"row"}
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Pagination
+          count={totalPages}
+          color="primary"
+          onChange={handleChangePage}
+          page={page}
+        />
+      </Stack>
     </>
   );
 };
--- newsAPI/src/context/NewsProvider.jsx
+++ newsAPI/src/context/NewsProvider.jsx
@@ -6,10 +6,8 @@ const NewsContext = createContext();
 const NewsProvider = ({ children }) => {
   const [category, setCategory] = useState("general");
   const [news, setNews] = useState([]);
-
-  const handleChangeCategory = (e) => {
-    setCategory(e.target.value);
-  };
+  const [page, setPage] = useState(1);
+  const [totalNews, setTotalNews] = useState(0);
 
   useEffect(() => {
     const APIQuery = async () => {
@@ -18,14 +16,47 @@ const NewsProvider = ({ children }) => {
       }`;
 
       const { data } = await axios(url);
-
+      console.log(data);
       setNews(data.articles);
+      setTotalNews(data.totalResults);
+      setPage(1);
     };
     APIQuery();
   }, [category]);
 
+  useEffect(() => {
+    const APIQuery = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&page=${page}&apiKey=${
+        import.meta.env.VITE_API_KEY
+      }`;
+
+      const { data } = await axios(url);
+      console.log(data);
+      setNews(data.articles);
+      setTotalNews(data.totalResults);
+    };
+    APIQuery();
+  }, [page]);
+
+  const handleChangeCategory = (e) => {
+    setCategory(e.target.value);
+  };
+
+  const handleChangePage = (e, value) => {
+    setPage(value);
+  };
+
   return (
-    <NewsContext.Provider value={{ category, handleChangeCategory, news }}>
+    <NewsContext.Provider
+      value={{
+        category,
+        handleChangeCategory,
+        news,
+        totalNews,
+        page,
+        handleChangePage,
+      }}
+    >
       {children}
     </NewsContext.Provider>
   );
