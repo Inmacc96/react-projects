9decb161f8f7b83f93dc626bf49dc1bba9285496
inmacc96 <inmacaballerocarrero@gmail.com>
2022-04-02 13:21:39 +0200

    Make the API request to get the news.

--- newsAPI/src/context/NewsProvider.jsx
+++ newsAPI/src/context/NewsProvider.jsx
@@ -1,13 +1,28 @@
 import { useState, useEffect, createContext } from "react";
+import axios from "axios";
 
 const NewsContext = createContext();
 
 const NewsProvider = ({ children }) => {
   const [category, setCategory] = useState("general");
+  const [news, setNews] = useState([]);
 
   const handleChangeCategory = (e) => {
     setCategory(e.target.value);
   };
+
+  useEffect(() => {
+    const APIQuery = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${
+        import.meta.env.VITE_API_KEY
+      }`;
+
+      const { data } = await axios(url);
+
+      setNews(data.articles);
+    };
+    APIQuery();
+  }, [category]);
 
   return (
     <NewsContext.Provider value={{ category, handleChangeCategory }}>
