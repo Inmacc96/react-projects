51c3fd80a391c19b65f789cc6726642a7e6ef98c
inmacc96 <inmacaballerocarrero@gmail.com>
2022-04-05 15:19:37 +0200

    Get drink categories from thecocktaildb API.

--- drinksfinder/src/context/CategoriesProvider.jsx
+++ drinksfinder/src/context/CategoriesProvider.jsx
@@ -1,10 +1,27 @@
 import { useState, useEffect, createContext } from "react";
+import axios from "axios";
 
 const CategoriesContext = createContext();
 
 const CategoriesProvider = ({ children }) => {
+    const [categories, setCategories] = useState([])
+  const getCategories = async () => {
+    try {
+      const url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list";
+
+      const { data } = await axios(url);
+
+      setCategories(data.drinks);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+      getCategories()
+  }, []);
   return (
-    <CategoriesContext.Provider value={{}}>
+    <CategoriesContext.Provider value={{categories}}>
       {children}
     </CategoriesContext.Provider>
   );
