b405930fbe6f0a28df7669f1a85f0f53839e8361
inmacc96 <inmacaballerocarrero@gmail.com>
2022-04-05 14:40:10 +0200

    Create drinksFinder app and add bootstrap and axios dependencies.

---
+++ drinksfinder/src/App.jsx
@@ -0,0 +1,13 @@
+import { Container } from "react-bootstrap";
+function App() {
+  return (
+    <>
+      <header className="py-5">
+        <h1>Drinks Finder</h1>
+      </header>
+      <Container className="mt-5">1</Container>
+    </>
+  );
+}
+
+export default App;
