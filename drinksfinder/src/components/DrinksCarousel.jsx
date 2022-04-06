cfc2cc3aba69d72b6b167ea344785a2c3abbf5e0
inmacc96 <inmacaballerocarrero@gmail.com>
2022-04-06 13:28:52 +0200

    Adjust the size of the carousel for different sizes.

--- drinksfinder/src/components/DrinksCarousel.jsx
+++ drinksfinder/src/components/DrinksCarousel.jsx
@@ -1,17 +1,27 @@
-import { Carousel, Image } from "react-bootstrap";
+import { Row, Col, Carousel, Image } from "react-bootstrap";
 import useDrinks from "../hooks/useDrinks";
 
 const DrinksCarousel = () => {
   const { randomDrinks } = useDrinks();
   return (
-    <Carousel fade className="mb-5">
-      {randomDrinks.map((drink) => (
-        <Carousel.Item key={drink.idDrink} interval={3000}>
-          <Image className='d-block w-100' src={drink.strDrinkThumb} alt={`${drink.strDrink}`} />
-          <Carousel.Caption><h1 className="text-danger">{drink.strDrink}</h1></Carousel.Caption>
-        </Carousel.Item>
-      ))}
-    </Carousel>
+    <Row className="justify-content-center">
+      <Col md={8} lg={4}>
+        <Carousel fade className="mb-5">
+          {randomDrinks.map((drink) => (
+            <Carousel.Item key={drink.idDrink} interval={3000}>
+              <Image
+                className="d-block w-100"
+                src={drink.strDrinkThumb}
+                alt={`${drink.strDrink}`}
+              />
+              <Carousel.Caption>
+                <h1>{drink.strDrink}</h1>
+              </Carousel.Caption>
+            </Carousel.Item>
+          ))}
+        </Carousel>
+      </Col>
+    </Row>
   );
 };
 
