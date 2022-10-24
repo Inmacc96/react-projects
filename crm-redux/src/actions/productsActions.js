50d05ac880d392b92846c9ad0f7a48f2df1c6908
inmacc96 <inmacaballerocarrero@gmail.com>
2022-10-24 17:10:03 +0200

    Add new Product to the products state.

--- crm-redux/src/actions/productsActions.js
+++ crm-redux/src/actions/productsActions.js
@@ -3,10 +3,26 @@ import { ADD_PRODUCT, ADD_PRODUCT_SUCCESS, ADD_PRODUCT_ERROR } from "../types";
 // Crear nuevos productos
 
 export function createNewProductAction(product) {
-  return () => {
-    // Lo guardamos en la BD
-    console.log(product);
-
-    //Llamamos al reducer para que actualice el state
+  return (dispatch) => {
+    dispatch(addProduct());
+    try {
+      dispatch(addProductSuccess(product));
+    } catch (err) {
+      dispatch(addProductError(true));
+    }
   };
 }
+
+const addProduct = () => ({
+  type: ADD_PRODUCT,
+  payload: true,
+});
+
+//Si el producto se guarda en la base de datos
+const addProductSuccess = (product) => ({
+  type: ADD_PRODUCT_SUCCESS,
+  payload: product,
+});
+
+//Si hubo un error
+const addProductError = () => {};
--- crm-redux/src/reducers/productsReducer.js
+++ crm-redux/src/reducers/productsReducer.js
@@ -1,3 +1,5 @@
+import { ADD_PRODUCT, ADD_PRODUCT_SUCCESS, ADD_PRODUCT_ERROR } from "../types";
+
 // Cada reducer tiene su propio state
 const initialState = {
   products: [],
@@ -7,6 +9,17 @@ const initialState = {
 
 export default function (state = initialState, action) {
   switch (action.type) {
+    case ADD_PRODUCT:
+      return {
+        ...state,
+        loading: action.payload,
+      };
+    case ADD_PRODUCT_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        products: [...state.products, action.payload],
+      };
     default:
       return state;
   }
