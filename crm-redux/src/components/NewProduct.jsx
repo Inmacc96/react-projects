521a2793340db5e678689cf40745944b31c8d5d4
inmacc96 <inmacaballerocarrero@gmail.com>
2022-10-24 12:08:43 +0200

    Format code.

--- crm-redux/src/components/NewProduct.jsx
+++ crm-redux/src/components/NewProduct.jsx
@@ -1,44 +1,46 @@
-
 const NewProduct = () => {
-    return (
-        <div className="row justify-content-center">
-            <div className="col-md-8">
-                <div className="card">
-                    <div className="card-body">
-                        <h2 className="text-center mb-4 font-weight-bold">
-                            Add New Product
-                        </h2>
+  return (
+    <div className="row justify-content-center">
+      <div className="col-md-8">
+        <div className="card">
+          <div className="card-body">
+            <h2 className="text-center mb-4 font-weight-bold">
+              Add New Product
+            </h2>
 
-                        <form>
-                            <div className="form-group">
-                                <label>Product Name</label>
-                                <input
-                                    type="text"
-                                    className="form-control"
-                                    placeholder="Product Name"
-                                    name="product-name" />
-                            </div>
+            <form>
+              <div className="form-group">
+                <label>Product Name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Product Name"
+                  name="product-name"
+                />
+              </div>
 
-                            <div className="form-group">
-                                <label>Product Price</label>
-                                <input
-                                    type="number"
-                                    className="form-control"
-                                    placeholder="Product Price"
-                                    name="product-price" />
-                            </div>
+              <div className="form-group">
+                <label>Product Price</label>
+                <input
+                  type="number"
+                  className="form-control"
+                  placeholder="Product Price"
+                  name="product-price"
+                />
+              </div>
 
-                            <button
-                                type="submit"
-                                className="btn btn-primary font-weight-bold text-uppercase d-bock w-100">
-                                Add Product
-                            </button>
-                        </form>
-                    </div>
-                </div>
-            </div>
+              <button
+                type="submit"
+                className="btn btn-primary font-weight-bold text-uppercase d-bock w-100"
+              >
+                Add Product
+              </button>
+            </form>
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default NewProduct
+export default NewProduct;
