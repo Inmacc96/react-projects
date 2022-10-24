19260c2f804a8f2d366323cd1d78f64c35aaf412
inmacc96 <inmacaballerocarrero@gmail.com>
2022-10-24 13:30:46 +0200

    Configure redux and create the app store.

--- crm-redux/src/App.jsx
+++ crm-redux/src/App.jsx
@@ -5,19 +5,26 @@ import EditProduct from "./components/EditProduct"
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 
+//REDUX
+import { Provider } from "react-redux"
+import store from "./store"
+
+
 function App() {
 
   return (
     <Router>
-      <Header />
+      <Provider store={store} >
+        <Header />
 
-      <div className="container mt-5">
-        <Routes>
-          <Route exact path="/" element={<Products />} />
-          <Route exact path="/products/new" element={<NewProduct />} />
-          <Route exact path="/products/edit/:id" element={<EditProduct />} />
-        </Routes>
-      </div>
+        <div className="container mt-5">
+          <Routes>
+            <Route exact path="/" element={<Products />} />
+            <Route exact path="/products/new" element={<NewProduct />} />
+            <Route exact path="/products/edit/:id" element={<EditProduct />} />
+          </Routes>
+        </div>
+      </Provider>
     </Router>
   )
 }
