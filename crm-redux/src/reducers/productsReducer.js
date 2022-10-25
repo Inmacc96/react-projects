41b74eb18e43bc92ca23bf889c74bce58d12465a
inmacc96 <inmacaballerocarrero@gmail.com>
2022-10-25 18:48:54 +0200

    Fix the functionality to delete the product from the products state.

--- crm-redux/src/reducers/productsReducer.js
+++ crm-redux/src/reducers/productsReducer.js
@@ -61,7 +61,7 @@ export default function (state = initialState, action) {
       return {
         ...state,
         products: state.products.filter(
-          (product) => product.id !== state.deletedProduct
+          (product) => product.id !== state.productDelete
         ),
         productDelete: null,
       };
