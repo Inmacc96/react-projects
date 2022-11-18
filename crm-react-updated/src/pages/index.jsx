6b9e75b7440f3e954cf356d88a85cce5f5122ea9
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-18 18:15:58 +0100

    Get loader data with useLoaderData

--- crm-react-updated/src/pages/index.jsx
+++ crm-react-updated/src/pages/index.jsx
@@ -1,8 +1,50 @@
+import { useLoaderData } from "react-router-dom";
+
 export function loader() {
-  return "Desde el loader";
+  const customers = [
+    {
+      id: 1,
+      name: "Juan",
+      phone: 102013313,
+      email: "juan@juan.com",
+      company: "Codigo Con Juan",
+    },
+    {
+      id: 2,
+      name: "Karen",
+      phone: 138198313,
+      email: "karen@juan.com",
+      company: "Codigo Con Juan",
+    },
+    {
+      id: 3,
+      name: "Josue",
+      phone: 31983913,
+      email: "josue@juan.com",
+      company: "Codigo Con Juan",
+    },
+    {
+      id: 4,
+      name: "Miguel",
+      phone: 319381983,
+      email: "miguel@juan.com",
+      company: "Codigo Con Juan",
+    },
+    {
+      id: 5,
+      name: "Pedro",
+      phone: 1398198938,
+      email: "pedro@juan.com",
+      company: "Codigo Con Juan",
+    },
+  ];
+  return customers;
 }
 
 const Index = () => {
+  const data = useLoaderData();
+
+  console.log(data);
   return (
     <>
       <h1 className="font-black text-4xl text-blue-900">Customers</h1>
