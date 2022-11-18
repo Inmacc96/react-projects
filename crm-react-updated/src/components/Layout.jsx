929a23719d017a10f3d8eeb34a15ca57131b3c2d
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-18 17:30:31 +0100

    Design the page Layout

--- crm-react-updated/src/components/Layout.jsx
+++ crm-react-updated/src/components/Layout.jsx
@@ -2,9 +2,16 @@ import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <div className="text-4xl font-bold">
-      <h1>CRM - React </h1>
-      <Outlet />
+    <div className="md:flex md:min-h-screen">
+      <aside className="md:w-1/4 bg-blue-900 px-5 py-10">
+        <h2 className="text-4xl font-black text-center text-white">
+          CRM - Customers
+        </h2>
+      </aside>
+
+      <main className="md:w-3/4 p-10 md:h-screen overflow-scroll">
+        <Outlet />
+      </main>
     </div>
   );
 };
