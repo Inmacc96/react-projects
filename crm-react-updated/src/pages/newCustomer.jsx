e300ee8cc85c4305e17a5bcd25d7d1b878422f74
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-18 18:47:41 +0100

    Design NewCustomer page

--- crm-react-updated/src/pages/newCustomer.jsx
+++ crm-react-updated/src/pages/newCustomer.jsx
@@ -1,5 +1,26 @@
+import { useNavigate } from "react-router-dom";
+
 const NewCustomer = () => {
-  return <div>NewCustomer</div>;
+  const navigate = useNavigate();
+  return (
+    <>
+      <h1 className="font-black text-4xl text-blue-900">New Customer</h1>
+      <p className="mt-3">Fill in all the fields to register a new client</p>
+
+      <div className="flex justify-end">
+        <button
+          className="bg-blue-800 text-white px-3 py-1 font-bold uppercase"
+          onClick={() => navigate(-1)}
+        >
+          Back to
+        </button>
+      </div>
+
+      <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10">
+        <p>Form here</p>
+      </div>
+    </>
+  );
 };
 
 export default NewCustomer;
