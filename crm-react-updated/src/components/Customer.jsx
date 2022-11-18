87c3450b3ebca75e4fafd1831ff8942a8dd1467e
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-18 18:34:02 +0100

    Iterate on customers and display their information

---
+++ crm-react-updated/src/components/Customer.jsx
@@ -0,0 +1,40 @@
+const Customer = ({ customer }) => {
+  const { name, email, phone, company, id } = customer;
+  return (
+    <tr className="border-b">
+      <td className="p-6 space-y-2">
+        <p className="text-2xl text-gray-800">{name}</p>
+        <p>{company}</p>
+      </td>
+
+      <td className="p-6">
+        <p className="text-gray-600">
+          <span className="text-gray-800 uppercase font-bold">Email: </span>
+          {email}
+        </p>
+        <p className="text-gray-600">
+          <span className="text-gray-800 uppercase font-bold">Phone: </span>
+          {phone}
+        </p>
+      </td>
+
+      <td className="p-6 flex gap-3">
+        <button
+          type="button"
+          className="text-blue-600 hover:text-blue-700 uppercase font-bold text-xs"
+        >
+          Edit
+        </button>
+
+        <button
+          type="button"
+          className="text-red-600 hover:text-red-700 uppercase font-bold text-xs"
+        >
+          Delete
+        </button>
+      </td>
+    </tr>
+  );
+};
+
+export default Customer;
--- crm-react-updated/src/pages/index.jsx
+++ crm-react-updated/src/pages/index.jsx
@@ -1,4 +1,5 @@
 import { useLoaderData } from "react-router-dom";
+import Customer from "../components/Customer";
 
 export function loader() {
   const customers = [
@@ -42,13 +43,32 @@ export function loader() {
 }
 
 const Index = () => {
-  const data = useLoaderData();
+  const customers = useLoaderData();
 
-  console.log(data);
   return (
     <>
       <h1 className="font-black text-4xl text-blue-900">Customers</h1>
       <p className="mt-3">Manage yours customers</p>
+
+      {customers.length ? (
+        <table className="w-full bg-white shadow mt-5 table-auto">
+          <thead className="bg-blue-800 text-white">
+            <tr>
+              <th className="p-2">Customer</th>
+              <th className="p-2">Info</th>
+              <th className="p-2">Actions</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {customers.map((customer) => (
+              <Customer key={customer.id} customer={customer} />
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <p className="text-center mt-10">There are no customers yet</p>
+      )}
     </>
   );
 };
