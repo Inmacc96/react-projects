0154a16fbb93ef2d1bc88771fed389bca3e41d77
inmacc96 <inmacaballerocarrero@gmail.com>
2022-11-18 17:00:17 +0100

    Create the router and define the first routes

--- crm-react-updated/src/App.jsx
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <h1 className="text-6xl font-bold"> CRM React - React Router DOM</h1>;
-}
-
-export default App;
--- crm-react-updated/src/main.jsx
+++ crm-react-updated/src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>Home</h1>,
+  },
+  {
+    path: "/aboutus",
+    element: <h1>About Us</h1>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
